b18ec6c57ed9c45d3bba842a3074e95484824ebf
Geraldine <geraldinesaul7@gmail.com>
2025-08-27 18:31:53 +0200

    Angular: todo list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
+
+interface Task {
+  title: string;
+  isDone: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
+
 export class App {
-  protected readonly title = signal('todo-list');
+  newTask: string = '';
+  tasks: Task[] = [];
+
+  addTask(): void {
+    if (this.newTask.trim()) {
+      this.tasks.push({title: this.newTask, isDone: false});
+      this.newTask = '';
+    }
+  }
 }
